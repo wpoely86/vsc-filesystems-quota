cd86fdc3ac066a9d16537fc3958df73f9608c16c
Jens Timmerman <jens.timmerman@ugent.be>
2016-05-03 15:53:15 +0200

    adapted to new vsc-install, + added missing dependencies and dependency links

--- setup.py
+++ setup.py
@@ -23,17 +23,27 @@ import vsc.install.shared_setup as shared_setup
 from vsc.install.shared_setup import ag
 
 PACKAGE = {
-    'version': '0.8.2',
+    'version': '0.9.1',
     'author': [ag],
     'maintainer': [ag],
     'excluded_pkgs_rpm': ['vsc', 'vsc.filesystem', 'vsc.filesystem.quota'],
     'install_requires': [
         'vsc-accountpage-clients',
+        'vsc-administration>=0.35',
         'vsc-base >= 1.6.6',
         'vsc-config >= 1.10',
         'vsc-filesystems >= 0.28',
         'vsc-utils >= 1.4.6',
     ],
+    'tests_require': ['mock'],
+    'dependency_links': [
+        "git+https://github.com/hpcugent/vsc-utils.git#egg=vsc-utils-1.8.2",
+        "git+https://github.com/hpcugent/vsc-filesystems.git#egg=vsc-filesystems-0.30.1",
+        "git+ssh://github.com/hpcugent/vsc-accountpage-clients.git#egg=vsc-accountpage-clients-0.7",
+        "git+ssh://github.com/hpcugent/vsc-administration.git#egg=vsc-administration-0.35",
+    ],
+
+
 }
 
 if __name__ == '__main__':
